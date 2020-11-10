b6c26bed2c02fe9b098e1d59244a6cdd1490ea99
Aryan Garg <gargaryan82000@gmail.com>
2020-11-10 12:24:14 +0530

    Added all the files and folders of calculator project.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var text = document.getElementById("input");
+
+function calcNumbers(result){
+    form.displayResult.value = form.displayResult.value+result;
+}
+
+function cleanValue(){
+    document.form.displayResult.value = "";
+}
